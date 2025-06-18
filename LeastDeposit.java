3e43e9c89eba31a866e89a72c7274ca867490c8e
EniyaaSK <eniyaask2005@gmail.com>
2025-06-18 21:23:04 +0530

    first commit

---
+++ LeastDeposit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LeastDeposit{
+    public static void main (String []args){
+        int token=0, cash=0,least=Integer.MAX_VALUE;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("How many no.of.Tokens Distributed:");
+    token=sc.nextInt();
+    while(token>0){
+        System.out.println("Amount to be deposited");
+        cash=sc.nextInt();
+        if(cash<=least){
+            least=cash;
+            token--;
+        }
+        
+    }
+    System.out.println("Least deposited amount:"+least);
+    sc.close();
+     
+    }
+}
---
+++ MaxPayers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MaxPayers{
+    public static void main (String []args){
+        double income=0.0,firstMax = Integer.MIN_VALUE,secondmax=Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
+
+        for(int payers =1;payers<=10;payers++){
+            System.out.println("tell us the income");
+            income=sc.nextInt();
+
+            if(firstMax<=income){
+                secondmax=firstMax;
+                firstMax=income;
+            }
+            else if(secondmax<=income && income==firstMax){
+                secondmax=income;
+
+            }
+           
+        }
+            System.out.println("Highest tax payers are:"+firstMax+"and"+secondmax);
+            sc.close();
+
+    }
+
+}
